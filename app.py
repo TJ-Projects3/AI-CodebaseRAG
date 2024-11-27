920e16d714b15a21c23e5dbd1bb753e98a2fb30f
TJ-Projects3 <tjprojects3@gmail.com>
2024-11-27 18:59:18 -0500

    Added chatbot functionality, pre-API connection

--- app.py
+++ app.py
@@ -1,8 +1,35 @@
 import streamlit as st
 
-with st.chat_message("user"):
+st.title("Echo Bot")
+
+with st.chat_message("assistant"):
     st.write("Hello Joseph 👋")
 
-    prompt = st.chat_input("Say something")
-if prompt:
-    st.write(f"{prompt}")
+# prompt = st.chat_input("Say something")
+# if prompt:
+#     st.write(f"User has sent the following prompt: {prompt}")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# React to user input
+# They call := the walrus operator XD
+if prompt := st.chat_input("What is up?"): # := assigns prompt to user input WHILE checking if user input is not None.
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+response = f"Echo: {prompt}"
+# Display assistant response in chat message container
+with st.chat_message("assistant"):
+    st.markdown(response)
+# Add assistant response to chat history
+st.session_state.messages.append({"role": "assistant", "content": response})
